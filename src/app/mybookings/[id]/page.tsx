f051f8ad9c0cf0703e0a550bdb325e88ae71b516
kmdshojib <kmd.shojib@gmail.com>
2024-10-20 00:44:32 +0600

    minor issue fixed with component My -Bookings

--- src/app/mybookings/[id]/page.tsx
+++ src/app/mybookings/[id]/page.tsx
@@ -1,16 +1,16 @@
-"use client"
-import { useParams } from 'next/navigation'
-import React from 'react'
+"use client";
+import { useParams } from "next/navigation";
+import React from "react";
 
-const myBookings = () => {
-  const id = useParams()
-  console.log(id)
+const MyBookings:React.FC = () => {
+  const { id } = useParams();
+  console.log(id);
 
   return (
     <div>
-      <p>This feature Will be implemented soon!</p>
+      <p>This feature will be implemented soon!</p>
     </div>
-  )
-}
+  );
+};
 
-export default myBookings
+export default MyBookings;
